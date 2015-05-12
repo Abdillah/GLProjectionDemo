a9cb7736df20dff24643fadc70e65d616944a5fe
Hernawan Fa'iz Abdillah <abdillah96.bu@gmail.com>
2015-05-12 08:25:30 +0700

    Install interaction key

--- OpenGL Projection/Program.cs
+++ OpenGL Projection/Program.cs
@@ -41,15 +41,59 @@ namespace OpenGL_Projection
                               new float []{ 2.5f, 2.0f, -0.5f},
                               new float []{-0.5f, 2.0f, -0.5f}};
 
+            int horizontalRotationAngle = 0;
+            int verticalRotationAngle = 0;
+            Vector3d horizontalNavigationVector = new Vector3d(0, 1, 0);
+            Vector3d verticalNavigationVector = new Vector3d(1, 0, 0);
+            bool isOrthographic = true;
+
             using (var game = new GameWindow())
             {
                 game.Width = 800;
                 game.Height = 600;
 
+                //Get the aspect ratio of the screen
+                double aspect_ratio = game.Width / (double)game.Height;
+                //Field of view of are camera
+                float fov = 1.0f;
+                //The nearest the camera can see, want to keep this number >= 0.1f else visible clipping ensues
+                float near_distance = 1.0f;
+                //The farthest the camera can see, depending on how far you want to draw this can be up to float.MaxValue
+                float far_distance = 1000.0f;
+
+                //Now we pass the parameters onto are matrix
+                OpenTK.Matrix4 perspective_matrix =
+                   OpenTK.Matrix4.CreatePerspectiveFieldOfView(fov, (float)aspect_ratio, near_distance, far_distance);
+
+                OpenTK.Matrix4 orthographic_matrix =
+                   OpenTK.Matrix4.CreateOrthographic(16, 12, near_distance, far_distance);
+                    
+
+
                 game.Load += (sender, e) =>
                 {
                     // setup settings, load textures, sounds
                     game.VSync = VSyncMode.On;
+                };
+
+                game.KeyUp += (sender, e) =>
+                {
+                    if (e.Key == Key.Enter)
+                    {
+                        isOrthographic = isOrthographic ? false : true;
+
+                        //Then we tell GL to use are matrix as the new Projection matrix.
+                        GL.MatrixMode(MatrixMode.Projection);
+
+                        if (isOrthographic)
+                        {
+                            GL.LoadMatrix(ref orthographic_matrix);
+                        }
+                        else
+                        {
+                            GL.LoadMatrix(ref perspective_matrix);
+                        }
+                    }
                 };
  
                 game.Resize += (sender, e) =>
@@ -58,25 +102,17 @@ namespace OpenGL_Projection
                     //you can have multiple viewports for splitscreen but for now just one.
                     GL.Viewport(0, 0, game.Width, game.Height);
 
-                    //Get the aspect ratio of the screen
-                    double aspect_ratio = game.Width / (double)game.Height;
-                    //Field of view of are camera
-                    float fov = 1.0f;
-                    //The nearest the camera can see, want to keep this number >= 0.1f else visible clipping ensues
-                    float near_distance = 1.0f;
-                    //The farthest the camera can see, depending on how far you want to draw this can be up to float.MaxValue
-                    float far_distance = 1000.0f;
-
-                    //Now we pass the parameters onto are matrix
-                    OpenTK.Matrix4 perspective_matrix =
-                       OpenTK.Matrix4.CreatePerspectiveFieldOfView(fov, (float)aspect_ratio, near_distance, far_distance);
-
-                    OpenTK.Matrix4 orthographic_matrix =
-                       OpenTK.Matrix4.CreateOrthographic(12, 9, near_distance, far_distance);
-                    
                     //Then we tell GL to use are matrix as the new Projection matrix.
                     GL.MatrixMode(MatrixMode.Projection);
-                    GL.LoadMatrix(ref orthographic_matrix);
+
+                    if (isOrthographic)
+                    {
+                        GL.LoadMatrix(ref orthographic_matrix);
+                    }
+                    else
+                    {
+                        GL.LoadMatrix(ref perspective_matrix);
+                    }
 					
                     Matrix4 lookat = Matrix4.LookAt(0, 0, 0, 0, 0, 0, 0, 1, 0);
 
@@ -90,6 +126,41 @@ namespace OpenGL_Projection
                     if (game.Keyboard[Key.Escape])
                     {
                         game.Exit();
+                    }
+                    if (game.Keyboard[Key.Left])
+                    {
+                        horizontalRotationAngle--;
+                        if (horizontalRotationAngle < 0)
+                        {
+                            horizontalRotationAngle = 360;
+                        }
+                    }
+                    if (game.Keyboard[Key.Right])
+                    {
+                        horizontalRotationAngle++;
+                        if (horizontalRotationAngle > 360)
+                        {
+                            horizontalRotationAngle = 0;
+                        }
+                    }
+                    if (game.Keyboard[Key.Up])
+                    {
+                        verticalRotationAngle--;
+                        if (verticalRotationAngle < 0)
+                        {
+                            verticalRotationAngle = 360;
+                        }
+                    }
+                    if (game.Keyboard[Key.Down])
+                    {
+                        verticalRotationAngle++;
+                        if (verticalRotationAngle > 360)
+                        {
+                            verticalRotationAngle = 0;
+                        }
+                    }
+                    if (game.Keyboard[Key.Enter])
+                    {
                     }
                 };
  
@@ -107,8 +178,10 @@ namespace OpenGL_Projection
                     //before that we have to set the coords they are currently (0, 0, 0) and are object
                     //will not be visible to us.
 
-                    GL.Translate(-3f, -3f, -10);
-                    GL.Rotate(-10, new Vector3d(-1, 1, -0.015));
+                    GL.Translate(-1f, -1f, -15);
+
+                    GL.Rotate(horizontalRotationAngle, horizontalNavigationVector);
+                    GL.Rotate(verticalRotationAngle, verticalNavigationVector);
 
                     //Draw Roof
                     GL.Begin(PrimitiveType.Triangles);
